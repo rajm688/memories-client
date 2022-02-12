ef4673e846ff330446a4e93d76637d40a943acd0
rajm688 <rajkumarm688@gmail.com>
2022-02-12 11:10:57 +0530

    comment added

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 
 export function Posts() {
   const posts = useSelector((state) => state.posts);
-  console.log(posts);
+  console.log(posts); // cannot view this consolelog in the console sir.
   return (
     <>
       <h1>Posts</h1>
